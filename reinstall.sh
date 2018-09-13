9414c3088152bd30bd8cd509d605e5c1d9fff493
Adam Berlin <aberlin@pivotal.io>
2018-09-13 10:31:50 -0400

    Make output more quiet.

--- reinstall.sh
+++ reinstall.sh
@@ -1,8 +1,8 @@
 echo "Reinstalling $PWD"
 
 echo "Re-running configure."
-./configure CC='ccache gcc -Qunused-arguments' --prefix=${PWD}/gpAux/greenplum-db-devel --disable-orca --with-libxml --with-python --with-perl --enable-mapreduce --enable-debug --enable-cassert CFLAGS=-O0 CPPFLAGS=-O0
+./configure --silent CC='ccache gcc -Qunused-arguments' --prefix=${PWD}/gpAux/greenplum-db-devel --disable-orca --with-libxml --with-python --with-perl --enable-mapreduce --enable-debug --enable-cassert CFLAGS=-O0 CPPFLAGS=-O0
 
 echo "Make and install"
-make clean &&  bear make -j6 install -s
+make clean -s && bear make -j6 -s && make install -s
 
