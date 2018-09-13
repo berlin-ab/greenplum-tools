d50c4dc64a292fc460222bb8ac7c328fe3ceb772
Adam Berlin <berlin.ab@gmail.com>
2018-09-13 10:53:07 -0400

    Extract configure from install.

--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -1,8 +1,5 @@
 echo "Reinstalling $PWD"
 
-echo "Re-running configure."
-./configure --silent CC='ccache gcc -Qunused-arguments' --prefix=${PWD}/gpAux/greenplum-db-devel --disable-orca --with-libxml --with-python --with-perl --enable-mapreduce --enable-debug --enable-cassert CFLAGS=-O0 CPPFLAGS=-O0
-
 echo "Make and install"
 make clean -s && bear make -j6 -s && make install -s
 
