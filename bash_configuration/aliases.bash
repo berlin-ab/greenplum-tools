490387ea7f826f0a2b6c20f861c52b87efbf46cf
Adam Berlin <aberlin@pivotal.io>
2018-09-14 00:20:39 -0400

    Add retagging for ctags to the aliases.

--- bash_configuration/aliases.bash
+++ bash_configuration/aliases.bash
@@ -2,3 +2,4 @@ alias 6="cd $HOME/workspace/gpdb/ && source gpAux/greenplum-db-devel/greenplum_p
 alias 5="cd $HOME/workspace/gpdb5/ && source gpAux/greenplum-db-devel/greenplum_path.sh && source gpAux/gpdemo/gpdemo-env.sh"
 alias 4="cd $HOME/workspace/gpdb4/ && source gpAux/greenplum-db-devel/greenplum_path.sh && source gpAux/gpdemo/gpdemo-env.sh"
 alias p='ps auxww | grep postgres'
+alias retag='ctags -e -R . TAGS'
