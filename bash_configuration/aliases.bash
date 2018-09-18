6d36293f2aec9004857bf61b8bada6ab3c324b1f
Adam Berlin <aberlin@pivotal.io>
2018-09-18 12:04:39 -0400

    Organize into functions.

--- bash_configuration/aliases.bash
+++ bash_configuration/aliases.bash
@@ -1,6 +1,36 @@
-alias 6="cd $HOME/workspace/gpdb/ && source gpAux/greenplum-db-devel/greenplum_path.sh && source gpAux/gpdemo/gpdemo-env.sh"
-alias 5="cd $HOME/workspace/gpdb5/ && source gpAux/greenplum-db-devel/greenplum_path.sh && source gpAux/gpdemo/gpdemo-env.sh"
-alias 4="cd $HOME/workspace/gpdb4/ && source gpAux/greenplum-db-devel/greenplum_path.sh && source gpAux/gpdemo/gpdemo-env.sh"
-alias p='ps auxww | grep postgres'
-alias retag='ctags -e -R . TAGS'
-alias merge="cd $HOME/workspace/gpdb-postgres-merge && source gpAux/greenplum-db-devel/greenplum_path.sh && source gpAux/gpdemo-env.sh"
+
+function setup_ctags_helpers {
+  alias retag='ctags -e -R . TAGS'
+}
+
+function setup_gpdb_navigation {
+  SOURCE_GREENPLUM='source gpAux/greenplum-db-devel/greenplum_path.sh'
+  SOURCE_DEMO_ENV='source gpAux/gpdemo/gpdemo-env.sh'
+
+  alias 6="cd $HOME/workspace/gpdb/ && $SOURCE_GREENPLUM && $SOURCE_DEMO_ENV"
+  alias 5="cd $HOME/workspace/gpdb5/ && $SOURCE_GREENPLUM && $SOURCE_DEMO_ENV"
+  alias 4="cd $HOME/workspace/gpdb4/ && $SOURCE_GREENPLUM && $SOURCE_DEMO_ENV"
+  alias merge="cd $HOME/workspace/gpdb-postgres-merge && $SOURCE_GREENPLUM && $SOURCE_DEMO_ENV"
+  alias tools="cd $HOME/workspace/gpdb-tools"
+}
+
+function disable_vim {
+  VIM_COMMAND="echo 'Disabled vim. Please use emacs.'"
+
+  alias vim=$VIM_COMMAND
+  alias vi=$VIM_COMMAND
+}
+
+function setup_debugging_tools {
+  alias p='ps auxww | grep postgres'
+}
+
+function main {
+  echo "Using $HOME/workspace/gpdb-tools."
+  setup_debugging_tools
+  setup_ctags_helpers
+  setup_gpdb_navigation
+  disable_vim
+}
+
+main
