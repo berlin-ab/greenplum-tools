c5e3635ed2c9bfe0bc57aab43d3a61db5850a28c
Adam Berlin <aberlin@pivotal.io>
2019-01-18 12:06:16 -0500

    Emacs configuration.

---
+++ emacs-config.el
@@ -0,0 +1,19 @@
+;; Setup projectile
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
+(projectile-mode +1)
+
+
+;; Current theme
+(load-theme 'misterioso)
+
+
+;; Load Postgres/Greenplum code style
+(load-file "~/workspace/gpdb/src/tools/editors/emacs.samples")
+
+;; Configure expand-region
+(require 'expand-region)
+(global-set-key (kbd "M-u") 'er/expand-region)
+(global-set-key (kbd "M-n") 'er/contract-region)
+(pending-delete-mode t)
