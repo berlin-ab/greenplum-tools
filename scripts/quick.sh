e287e03f5966306854e864d3ade358e4a4fd64e7
Adam Berlin <aberlin@pivotal.io>
2019-02-04 16:44:51 -0500

    Quickly re-install gpdb silently.

---
+++ scripts/quick.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+change_to_project_root() {
+  local project_root=$(git rev-parse --show-toplevel);
+  pushd "$project_root";
+}
+
+change_to_original_directory() {
+  popd;
+}
+
+build() {
+    make install -s
+}
+
+message_rebuilding() {
+  echo "Rebuilding gpdb."
+}
+
+function main {
+    change_to_project_root
+    message_rebuilding
+    build
+    change_to_original_directory
+}
+
+main
