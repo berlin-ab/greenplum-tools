b0822d02e17a83491432bd6eb216a1b91fe11b84
Adam Berlin <aberlin@pivotal.io>
2019-02-04 16:44:21 -0500

    Auto-re-install and jump to isolation2 directory

--- scripts/isolation2.sh
+++ scripts/isolation2.sh
@@ -1,7 +1,37 @@
 #!/usr/bin/env bash
 
-make install -s -C ../regress && \
-    make install -s && \
-    ./pg_isolation2_regress --init-file ../regress/init_file $1
 
-cat regression.diffs
+change_to_project_root() {
+  local project_root=$(git rev-parse --show-toplevel);
+  pushd "$project_root";
+}
+
+
+change_to_isolation2_directory() {
+    pushd "src/test/isolation2"
+}
+
+
+quick_reinstall() {
+    make install -s
+}
+
+
+run_isolation2() {
+    make install -s -C ../regress && \
+	make install -s && \
+       ./pg_isolation2_regress --init-file ../regress/init_file $1
+    
+    test regression.diffs && cat regression.diffs
+}
+
+
+main() {
+    change_to_project_root
+    quick_reinstall
+    change_to_isolation2_directory
+    case 
+    run_isolation2 $1
+}
+
+main "$@"
