3ec41c42bc9c2b09fadeb824bab8b6fbc7ba85de
Adam Berlin <aberlin@pivotal.io>
2019-02-06 16:52:49 -0500

    Enable ssl.

--- scripts/reconfigure.sh
+++ scripts/reconfigure.sh
@@ -1,5 +1,10 @@
 echo "Reconfiguring $PWD"
 
 echo "Re-running configure."
-./configure --silent CC='ccache gcc' --prefix=${PWD}/gpAux/greenplum-db-devel --disable-orca --with-libxml --with-python --with-perl --enable-mapreduce --enable-debug --enable-cassert --enable-tap-tests CFLAGS=-O0 CPPFLAGS=-O0
+./configure --silent CC='ccache gcc' \
+	    --prefix=${PWD}/gpAux/greenplum-db-devel \
+	    --disable-orca --with-libxml --with-python \
+	    --with-perl --enable-mapreduce --enable-debug \
+	    --enable-cassert --enable-tap-tests --with-openssl \
+	    CFLAGS=-O0 CPPFLAGS=-O0
 
