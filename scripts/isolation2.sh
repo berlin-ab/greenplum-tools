5e80a547bee7afcb4c7c2f52838a1e5959890150
Adam Berlin <aberlin@pivotal.io>
2019-02-08 12:49:14 -0500

    Run multiple tests at once.

--- scripts/isolation2.sh
+++ scripts/isolation2.sh
@@ -6,7 +6,7 @@ source "$(dirname $0)/lib/shared.bash";
 run_isolation2() {
     make install -s -C ../regress && \
 	make install -s && \
-       ./pg_isolation2_regress --init-file ../regress/init_file $1
+       ./pg_isolation2_regress --init-file ../regress/init_file $@
     
     (test -e regression.diffs && cat regression.diffs) || true
 }
@@ -17,7 +17,7 @@ main() {
     quick_reinstall
     change_to_isolation2_directory
 
-    run_isolation2 $1
+    run_isolation2 "$@"
 }
 
 main "$@"
