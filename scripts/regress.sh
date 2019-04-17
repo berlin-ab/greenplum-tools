7b6a2895384f18b0d79f5877b9c6c96f5b4912b0
Adam Berlin <aberlin@pivotal.io>
2019-04-17 14:22:33 +0000

    Allow for running multiple regression tests at once.

--- scripts/regress.sh
+++ scripts/regress.sh
@@ -6,7 +6,7 @@ set -e;
 source "$(dirname $0)/lib/shared.bash";
 
 
-REGRESSION_FILE_NAME=$1
+REGRESSION_FILE_NAME=$@
 
 
 function switch_to_regression_directory {
@@ -16,8 +16,13 @@ function switch_to_regression_directory {
 
 
 function run_regression_test {
+    #local print_failure_diffs='--print-failure-diffs'; # for 6x+
+    local print_failure_diffs=''; # for 5x
+    
     quick_reinstall && \
-	./pg_regress --init-file init_file $REGRESSION_FILE_NAME
+	./pg_regress --init-file init_file \
+		     $print_failure_diffs \
+		     $REGRESSION_FILE_NAME
 }
 
 
@@ -29,7 +34,7 @@ function show_output {
 
 function main {
     switch_to_regression_directory
-    run_regression_test
+    run_regression_test "$@"
     show_output
 }
 
