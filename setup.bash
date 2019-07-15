239d090e86de6c6b4e13139041a5ed7e2584bf11
Adam Berlin <aberlin@pivotal.io>
2019-07-15 10:50:51 -0400

    Install dotfiles automatically. Include .psqlrc

--- setup.bash
+++ setup.bash
@@ -8,9 +8,11 @@ main() {
 
 	source "$tools_directory/bash_configuration/aliases.bash"
 	source "$tools_directory/bash_configuration/variables.bash"
+	source "$tools_directory/dotfiles/setup.bash"
 
 	initialize_variables $tools_directory
 	initialize_aliases
+	setup_dotfiles $tools_directory
 }
 
 main
