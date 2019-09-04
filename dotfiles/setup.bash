e3517341b01d6477a650b4830d0e79c7a5884b53
Adam Berlin <aberlin@pivotal.io>
2019-09-04 15:08:05 -0400

    Formatting

--- dotfiles/setup.bash
+++ dotfiles/setup.bash
@@ -3,18 +3,18 @@
 #
 
 install_dotfile() {
-    local tools_directory=$1
-    local dotfile_name=$2
+	local tools_directory=$1
+	local dotfile_name=$2
 
-    ln -Ffsv "${tools_directory}/dotfiles/${dotfile_name}" "$HOME/.${dotfile_name}"
+	ln -Ffsv "${tools_directory}/dotfiles/${dotfile_name}" "$HOME/.${dotfile_name}"
 }
 
 
 setup_dotfiles() {
-    local tools_directory=$1
-    local dotfiles=(psqlrc gitignore_global emacs)
+	local tools_directory=$1
+	local dotfiles=(psqlrc gitignore_global emacs)
 
-    for dotfile_name in ${dotfiles[*]}; do
-	install_dotfile "$tools_directory" "${dotfile_name}"
-    done
+	for dotfile_name in ${dotfiles[*]}; do
+		install_dotfile "$tools_directory" "${dotfile_name}"
+	done
 }
